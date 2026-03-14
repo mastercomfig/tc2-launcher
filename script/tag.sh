a7f33843e49f5366796b54fe63e822320a238c5b
mastercoms <mastercoms@tuta.io>
2026-03-13 22:12:00 -0400

    add release workflow

---
+++ script/tag.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#
+# Run script within the directory
+BIN_DIR=$(dirname "$(readlink -fn "$0")")
+cd "${BIN_DIR}" || exit 2
+
+set -e
+
+git fetch --tags origin
+
+VERSION=$(git describe --tags)
+
+printf "Release ${VERSION}" > "notes.txt"
+gh release create ${VERSION} \
+    "dist/*" \
+    --title "${VERSION}" \
+    -F "notes.txt" \
+    --prerelease \
+    --verify-tag \
+    --fail-on-no-commits
+rm notes.txt
