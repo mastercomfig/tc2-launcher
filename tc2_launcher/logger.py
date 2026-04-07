0b77344e5da2c32c71a3a0933dd6e0bffb4aef3b
mastercoms <mastercoms@tuta.io>
2026-04-07 14:36:29 -0400

    add user data scrubber

--- tc2_launcher/logger.py
+++ tc2_launcher/logger.py
@@ -8,6 +8,25 @@ from tc2_launcher.utils import DEV_INSTANCE
 root_log = None
 
 
+class UserDataScrubberFormatter(logging.Formatter):
+    def __init__(self, fmt=None, datefmt=None):
+        super().__init__(fmt, datefmt=datefmt)
+        home = os.path.expanduser("~")
+        if os.name == "nt":
+            self.home_win = home
+        else:
+            self.home_win = None
+        self.home_posix = home.replace("\\", "/")
+
+    def format(self, record):
+        s = super().format(record)
+        if self.home_win:
+            s = s.replace(self.home_win, "~")
+        if self.home_posix:
+            s = s.replace(self.home_posix, "~")
+        return s
+
+
 def setup_logger(log_folder: Path):
     global root_log
 
@@ -15,7 +34,7 @@ def setup_logger(log_folder: Path):
 
     format_string = "%(asctime)s [%(levelname)-5.5s]  %(message)s"
     date_format = "%d-%b-%y %H:%M:%S"
-    log_formatter = logging.Formatter(format_string, datefmt=date_format)
+    log_formatter = UserDataScrubberFormatter(format_string, datefmt=date_format)
 
     root_log = logging.getLogger("tc2_launcher")
 
